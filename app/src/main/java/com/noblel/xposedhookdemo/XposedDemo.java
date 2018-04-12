0cc46d007bd68d59e5ea2fbba9682ee7b4e16473
noblel <357384397@qq.com>
2018-04-12 17:16:02 +0800

    Initial commit

---
+++ app/src/main/java/com/noblel/xposedhookdemo/XposedDemo.java
@@ -0,0 +1,15 @@
+package com.noblel.xposedhookdemo;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * @author Noblel
+ */
+public class XposedDemo implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("Loaded app: " + lpparam.packageName);
+    }
+}
